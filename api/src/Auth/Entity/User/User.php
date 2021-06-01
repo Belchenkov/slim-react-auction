5252d6dd659b06d7fe5fc3335464acd5a7a6cfad
Aleksey Belchenkov <u608110@gmail.com>
2021-06-01 12:43:12 +0400

    Added other custom types

--- api/src/Auth/Entity/User/User.php
+++ api/src/Auth/Entity/User/User.php
@@ -21,14 +21,32 @@ class User
      * @ORM\Id
      */
     private Id $id;
+    /**
+     * @ORM\Column(type="datetime_immutable")
+     */
     private DateTimeImmutable $date;
+    /**
+     * @ORM\Column(type="auth_user_email", unique=true)
+     */
     private Email $email;
+    /**
+     * @ORM\Column(type="string", nullable=true)
+     */
     private ?string $passwordHash = null;
+    /**
+     * @ORM\Column(type="auth_user_status", length=16)
+     */
     private Status $status;
     private ?Token $joinConfirmToken = null;
     private ?Token $passwordResetToken = null;
+    /**
+     * @ORM\Column(type="auth_user_email", nullable=true)
+     */
     private ?Email $newEmail = null;
     private ?Token $newEmailToken = null;
+    /**
+     * @ORM\Column(type="auth_user_role", length=16)
+     */
     private Role $role;
     private ArrayObject $networks;
 
