32dfd5b5421b01bcd259d8d8a75ce04c555e0d65
Aleksey Belchenkov <u608110@gmail.com>
2021-06-04 18:40:39 +0400

    Removed useless factory

--- api/config/common/auth.php
+++ api/config/common/auth.php
@@ -7,8 +7,6 @@ use App\Auth\Entity\User\User;
 use App\Auth\Entity\User\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\EntityRepository;
-use App\Auth\Service\JoinConfirmationSender;
-use App\Frontend\FrontendUrlGenerator;
 use Psr\Container\ContainerInterface;
 
 return [
@@ -18,15 +16,5 @@ return [
         /** @var EntityRepository $repo */
         $repo = $em->getRepository(User::class);
         return new UserRepository($em, $repo);
-    },
-
-    JoinConfirmationSender::class => function (ContainerInterface $container): JoinConfirmationSender {
-        /** @var Swift_Mailer $mailer */
-        $mailer = $container->get(Swift_Mailer::class);
-
-        /** @var FrontendUrlGenerator $frontend */
-        $frontend = $container->get(FrontendUrlGenerator::class);
-
-        return new JoinConfirmationSender($mailer, $frontend);
     },
 ];
