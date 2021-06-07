1a04c41b370b2fdf4719bf23c10f99f81b81fd2e
Aleksey Belchenkov <u608110@gmail.com>
2021-06-07 13:47:07 +0400

    Added empty response

--- api/src/Http/Action/V1/Auth/Join/RequestAction.php
+++ api/src/Http/Action/V1/Auth/Join/RequestAction.php
@@ -7,6 +7,7 @@ namespace App\Http\Action\V1\Auth\Join;
 use App\Auth\Command\JoinByEmail\Request\Command;
 use App\Auth\Command\JoinByEmail\Request\Handler;
 use App\Http\JsonResponse;
+use App\Http\EmptyResponse;
 use DomainException;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
@@ -34,7 +35,7 @@ class RequestAction implements RequestHandlerInterface
 
         try {
             $this->handler->handle($command);
-            return new JsonResponse(new \stdClass(), 201);
+            return new EmptyResponse(201);
         } catch (DomainException $exception) {
             return new JsonResponse(['message' => $exception->getMessage()], 409);
         }
