61e792154cf3992041afa742a90e9c545f371381
Aleksey Belchenkov <u608110@gmail.com>
2021-06-09 13:59:19 +0400

    Extracted validation handling

--- api/config/middleware.php
+++ api/config/middleware.php
@@ -9,6 +9,7 @@ use Slim\Middleware\ErrorMiddleware;
 return static function (App $app): void {
     $app->add(Middleware\DomainExceptionHandler::class);
     $app->add(Middleware\ClearEmptyInput::class);
+    $app->add(Middleware\ValidationExceptionHandler::class);
     $app->addBodyParsingMiddleware();
     $app->add(ErrorMiddleware::class);
 };
